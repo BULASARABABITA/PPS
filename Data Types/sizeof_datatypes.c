098ac3bada8698a8eae04f7f3b4565e0cafda591
BULASARABABITA <85932119+BULASARABABITA@users.noreply.github.com>
2021-07-16 19:06:13 +0530

    Add files via upload

---
+++ Data Types/sizeof_datatypes.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+	printf("\nsize of int = %d bytes",sizeof(int));
+	printf("\nsize of char = %d bytes",sizeof(char));
+	printf("\nsize of float = %d bytes",sizeof(float));
+	printf("\nsize of double = %d bytes",sizeof(double));
+	printf("\nsize of long double = %d bytes",sizeof(long double));
+	printf("\nsize of long = %d bytes",sizeof(long));
+}
