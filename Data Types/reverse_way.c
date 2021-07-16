e4e85bece56086a4efbe57e31aae5f5a73c3e9cb
BULASARABABITA <85932119+BULASARABABITA@users.noreply.github.com>
2021-07-16 19:00:31 +0530

    Add files via upload

---
+++ Data Types/reverse_way.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	char ch1,ch2,ch3;
+	printf("enter the value of ch1:");
+	scanf("\n%c",&ch1);
+	printf("enter the value of ch2:");
+	scanf("\n%c",&ch2);
+	printf("enter the value of ch3:");
+	scanf("\n%c",&ch3);
+	printf("%c %c %c",ch3,ch2,ch1);
+}
