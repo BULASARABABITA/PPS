0dc55d81d5c3daf989208b50dc29f67b67eab68d
BULASARABABITA <85932119+BULASARABABITA@users.noreply.github.com>
2021-07-18 11:12:54 +0530

    Add files via upload

---
+++ if_else/compare_2num.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+	int num1,num2;
+	printf("\nEnter the values:");
+	scanf("%d%d",&num1,&num2);
+	if(num1>num2)
+	printf("%d is big",num1);
+	if(num2>num1)
+    printf("%d is big",num2);
+    else
+    printf("both are equal");
+    
+    return 0;
+    
+	      
+	      
+	
+}
