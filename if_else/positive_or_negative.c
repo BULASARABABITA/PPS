bad8aa084bc41c4ea6bc415999a02eac26a3b581
BULASARABABITA <85932119+BULASARABABITA@users.noreply.github.com>
2021-07-18 11:09:03 +0530

    Add files via upload

---
+++ if_else/positive_or_negative.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+	int num;
+	printf("\nEnter the value of num:");
+	scanf("%d",&num);
+	if(num>0)
+	   printf("%d is positive",num);
+	else
+	   printf("%d is negative",num);   
+}
