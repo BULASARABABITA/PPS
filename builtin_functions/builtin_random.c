ea39e7df4051b6d2466dfc4b203433b7044665ba
BULASARABABITA <85932119+BULASARABABITA@users.noreply.github.com>
2021-07-20 07:25:47 +0530

    Add files via upload

---
+++ builtin_functions/builtin_random.c
@@ -0,0 +1,11 @@
+/* Generate a random number using built-in Function*/
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+	printf("A random number %d", rand() % 5000); 
+}
+	                    
+	
+
+
