acd24c18e0ae2dac25d292a046e8f4f1a928bbfd
BULASARABABITA <85932119+BULASARABABITA@users.noreply.github.com>
2021-07-20 07:36:27 +0530

    Add files via upload

---
+++ if_else/big_2num.c
@@ -0,0 +1,13 @@
+/* find biggest of 2 numbers.*/
+#include<stdio.h>
+int main()
+{
+	int a,b;
+	printf("\nEnter the values of a and b");
+	scanf("%d%d",&a,&b);
+	if(a>b)
+	printf("a is big");
+	else
+	printf("b is big");
+	return 0;
+}
